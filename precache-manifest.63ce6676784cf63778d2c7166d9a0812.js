c289196c6a779e5bbb281228cf7043970ee17f52
ankita21prasad <ankita2108prasad@gmail.com>
2020-05-02 19:57:24 +0530

    Updates

---
+++ precache-manifest.63ce6676784cf63778d2c7166d9a0812.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5610153dcda9dae5ce21c945d455ff96",
+    "url": "/photogrid/index.html"
+  },
+  {
+    "revision": "d002ca692ff6dd308287",
+    "url": "/photogrid/static/css/main.b493a473.chunk.css"
+  },
+  {
+    "revision": "08ba3bb969007372c863",
+    "url": "/photogrid/static/js/2.813f73aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/photogrid/static/js/2.813f73aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d002ca692ff6dd308287",
+    "url": "/photogrid/static/js/main.d2e06ea7.chunk.js"
+  },
+  {
+    "revision": "fe9c5f23fdf163852a80",
+    "url": "/photogrid/static/js/runtime-main.280e348c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/photogrid/static/media/logo.5d5d9eef.svg"
+  }
+]);
